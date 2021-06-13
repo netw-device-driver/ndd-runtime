cb955ef84c38bb0f3fb15324eb03afeafc8369c2
henderiw <wim.henderickx@gmail.com>
2021-06-13 07:58:57 +0200

    added resource

---
+++ pkg/resource/interfaces.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2020 Wim Henderickx.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resource
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	nddv1 "github.com/netw-device-driver/ndd-runtime/apis/common/v1"
+)
+
+// A Conditioned may have conditions set or retrieved. Conditions are typically
+// indicate the status of both a resource and its reconciliation process.
+type Conditioned interface {
+	SetConditions(c ...nddv1.Condition)
+	GetCondition(nddv1.ConditionKind) nddv1.Condition
+}
+
+// An Object is a Kubernetes object.
+type Object interface {
+	metav1.Object
+	runtime.Object
+}
+
+// A ProviderConfig configures a Crossplane provider.
+type ProviderConfig interface {
+	Object
+
+	Conditioned
+}
