d8fc4d6a39627bbb41002bbc339bc06202886e5d
henderiw <wim.henderickx@gmail.com>
2021-06-15 21:28:26 +0200

    added typedreference

--- apis/common/v1/resource.go
+++ apis/common/v1/resource.go
@@ -16,8 +16,28 @@ limitations under the License.
 
 package v1
 
+import "k8s.io/apimachinery/pkg/types"
+
 // A Reference to a named object.
 type Reference struct {
 	// Name of the referenced object.
 	Name string `json:"name"`
+}
+
+// A TypedReference refers to an object by Name, Kind, and APIVersion. It is
+// commonly used to reference cluster-scoped objects or objects where the
+// namespace is already known.
+type TypedReference struct {
+	// APIVersion of the referenced object.
+	APIVersion string `json:"apiVersion"`
+
+	// Kind of the referenced object.
+	Kind string `json:"kind"`
+
+	// Name of the referenced object.
+	Name string `json:"name"`
+
+	// UID of the referenced object.
+	// +optional
+	UID types.UID `json:"uid,omitempty"`
 }
