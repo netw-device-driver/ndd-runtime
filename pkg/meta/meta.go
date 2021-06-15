731138774a8793df178e435acf597826df72cbf1
henderiw <wim.henderickx@gmail.com>
2021-06-15 21:45:04 +0200

    add meta functions

--- pkg/meta/meta.go
+++ pkg/meta/meta.go
@@ -18,7 +18,68 @@ package meta
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	nddv1 "github.com/netw-device-driver/ndd-runtime/apis/common/v1"
+	"github.com/pkg/errors"
 )
+
+// TypedReferenceTo returns a typed object reference to the supplied object,
+// presumed to be of the supplied group, version, and kind.
+func TypedReferenceTo(o metav1.Object, of schema.GroupVersionKind) *nddv1.TypedReference {
+	v, k := of.ToAPIVersionAndKind()
+	return &nddv1.TypedReference{
+		APIVersion: v,
+		Kind:       k,
+		Name:       o.GetName(),
+		UID:        o.GetUID(),
+	}
+}
+
+// AsOwner converts the supplied object reference to an owner reference.
+func AsOwner(r *nddv1.TypedReference) metav1.OwnerReference {
+	return metav1.OwnerReference{
+		APIVersion: r.APIVersion,
+		Kind:       r.Kind,
+		Name:       r.Name,
+		UID:        r.UID,
+	}
+}
+
+// AsController converts the supplied object reference to a controller
+// reference. You may also consider using metav1.NewControllerRef.
+func AsController(r *nddv1.TypedReference) metav1.OwnerReference {
+	c := true
+	ref := AsOwner(r)
+	ref.Controller = &c
+	return ref
+}
+
+// AddOwnerReference to the supplied object' metadata. Any existing owner with
+// the same UID as the supplied reference will be replaced.
+func AddOwnerReference(o metav1.Object, r metav1.OwnerReference) {
+	refs := o.GetOwnerReferences()
+	for i := range refs {
+		if refs[i].UID == r.UID {
+			refs[i] = r
+			o.SetOwnerReferences(refs)
+			return
+		}
+	}
+	o.SetOwnerReferences(append(refs, r))
+}
+
+// AddControllerReference to the supplied object's metadata. Any existing owner
+// with the same UID as the supplied reference will be replaced. Returns an
+// error if the supplied object is already controlled by a different owner.
+func AddControllerReference(o metav1.Object, r metav1.OwnerReference) error {
+	if c := metav1.GetControllerOf(o); c != nil && c.UID != r.UID {
+		return errors.Errorf("%s is already controlled by %s %s (UID %s)", o.GetName(), c.Kind, c.Name, c.UID)
+	}
+
+	AddOwnerReference(o, r)
+	return nil
+}
 
 // AddFinalizer to the supplied Kubernetes object's metadata.
 func AddFinalizer(o metav1.Object, finalizer string) {
