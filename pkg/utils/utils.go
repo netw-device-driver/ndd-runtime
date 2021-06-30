120ef2eded93abd8bf4720d05c1c4f408ed75ada
henderiw <wim.henderickx@gmail.com>
2021-06-30 12:10:43 +0200

    add util pkg

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package util
+package utils
 
 // BoolPtr return pointer to boolean
 func BoolPtr(b bool) *bool { return &b }
