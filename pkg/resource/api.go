26bebb14bb269e31fda04dd35939f4f28c0c4e20
henderiw <wim.henderickx@gmail.com>
2021-07-26 06:34:38 +0200

    changed api

--- pkg/resource/api.go
+++ pkg/resource/api.go
@@ -34,53 +34,6 @@ const (
 	errUpdateObject = "cannot update object"
 )
 
-// An APIUpdatingApplicator applies changes to an object by either creating or
-// updating it in a Kubernetes API server.
-type APIUpdatingApplicator struct {
-	client client.Client
-}
-
-// NewAPIUpdatingApplicator returns an Applicator that applies changes to an
-// object by either creating or updating it in a Kubernetes API server.
-func NewAPIUpdatingApplicator(c client.Client) *APIUpdatingApplicator {
-	return &APIUpdatingApplicator{client: c}
-}
-
-// Apply changes to the supplied object. The object will be created if it does
-// not exist, or updated if it does.
-func (a *APIUpdatingApplicator) Apply(ctx context.Context, o client.Object, ao ...ApplyOption) error {
-	m, ok := o.(Object)
-	if !ok {
-		return errors.New("cannot access object metadata")
-	}
-
-	if m.GetName() == "" && m.GetGenerateName() != "" {
-		return errors.Wrap(a.client.Create(ctx, o), "cannot create object")
-	}
-
-	current := o.DeepCopyObject().(client.Object)
-
-	err := a.client.Get(ctx, types.NamespacedName{Name: m.GetName(), Namespace: m.GetNamespace()}, current)
-	if kerrors.IsNotFound(err) {
-		// TODO(negz): Apply ApplyOptions here too?
-		return errors.Wrap(a.client.Create(ctx, m), "cannot create object")
-	}
-	if err != nil {
-		return errors.Wrap(err, "cannot get object")
-	}
-
-	for _, fn := range ao {
-		if err := fn(ctx, current, m); err != nil {
-			return err
-		}
-	}
-
-	// NOTE(hasheddan): we must set the resource version of the desired object
-	// to that of the current or the update will always fail.
-	m.SetResourceVersion(current.(metav1.Object).GetResourceVersion())
-	return errors.Wrap(a.client.Update(ctx, m), "cannot update object")
-}
-
 // An APIPatchingApplicator applies changes to an object by either creating or
 // patching it in a Kubernetes API server.
 type APIPatchingApplicator struct {
@@ -114,7 +67,7 @@ func (a *APIPatchingApplicator) Apply(ctx context.Context, o client.Object, ao .
 
 	err := a.client.Get(ctx, types.NamespacedName{Name: m.GetName(), Namespace: m.GetNamespace()}, o)
 	if kerrors.IsNotFound(err) {
-		// TODO(negz): Apply ApplyOptions here too?
+		// TODO: Apply ApplyOptions here too?
 		return errors.Wrap(a.client.Create(ctx, o), "cannot create object")
 	}
 	if err != nil {
@@ -127,7 +80,7 @@ func (a *APIPatchingApplicator) Apply(ctx context.Context, o client.Object, ao .
 		}
 	}
 
-	// TODO: &patch{o} was &patch{desired}
+	// TODO: Allow callers to override the kind of patch used.
 	return errors.Wrap(a.client.Patch(ctx, o, &patch{desired.(client.Object)}), "cannot patch object")
 }
 
@@ -135,6 +88,54 @@ type patch struct{ from client.Object }
 
 func (p *patch) Type() types.PatchType                { return types.MergePatchType }
 func (p *patch) Data(_ client.Object) ([]byte, error) { return json.Marshal(p.from) }
+
+
+// An APIUpdatingApplicator applies changes to an object by either creating or
+// updating it in a Kubernetes API server.
+type APIUpdatingApplicator struct {
+	client client.Client
+}
+
+// NewAPIUpdatingApplicator returns an Applicator that applies changes to an
+// object by either creating or updating it in a Kubernetes API server.
+func NewAPIUpdatingApplicator(c client.Client) *APIUpdatingApplicator {
+	return &APIUpdatingApplicator{client: c}
+}
+
+// Apply changes to the supplied object. The object will be created if it does
+// not exist, or updated if it does.
+func (a *APIUpdatingApplicator) Apply(ctx context.Context, o client.Object, ao ...ApplyOption) error {
+	m, ok := o.(Object)
+	if !ok {
+		return errors.New("cannot access object metadata")
+	}
+
+	if m.GetName() == "" && m.GetGenerateName() != "" {
+		return errors.Wrap(a.client.Create(ctx, o), "cannot create object")
+	}
+
+	current := o.DeepCopyObject().(client.Object)
+
+	err := a.client.Get(ctx, types.NamespacedName{Name: m.GetName(), Namespace: m.GetNamespace()}, current)
+	if kerrors.IsNotFound(err) {
+		// TODO: Apply ApplyOptions here too?
+		return errors.Wrap(a.client.Create(ctx, m), "cannot create object")
+	}
+	if err != nil {
+		return errors.Wrap(err, "cannot get object")
+	}
+
+	for _, fn := range ao {
+		if err := fn(ctx, current, m); err != nil {
+			return err
+		}
+	}
+
+	// NOTE: we must set the resource version of the desired object
+	// to that of the current or the update will always fail.
+	m.SetResourceVersion(current.(metav1.Object).GetResourceVersion())
+	return errors.Wrap(a.client.Update(ctx, m), "cannot update object")
+}
 
 // An APIFinalizer adds and removes finalizers to and from a resource.
 type APIFinalizer struct {
@@ -163,4 +164,20 @@ func (a *APIFinalizer) RemoveFinalizer(ctx context.Context, obj Object) error {
 	}
 	meta.RemoveFinalizer(obj, a.finalizer)
 	return errors.Wrap(IgnoreNotFound(a.client.Update(ctx, obj)), errUpdateObject)
+}
+
+// A FinalizerFns satisfy the Finalizer interface.
+type FinalizerFns struct {
+	AddFinalizerFn    func(ctx context.Context, obj Object) error
+	RemoveFinalizerFn func(ctx context.Context, obj Object) error
+}
+
+// AddFinalizer to the supplied resource.
+func (f FinalizerFns) AddFinalizer(ctx context.Context, obj Object) error {
+	return f.AddFinalizerFn(ctx, obj)
+}
+
+// RemoveFinalizer from the supplied resource.
+func (f FinalizerFns) RemoveFinalizer(ctx context.Context, obj Object) error {
+	return f.RemoveFinalizerFn(ctx, obj)
 }
--- pkg/resource/doc.go
+++ pkg/resource/doc.go
@@ -14,12 +14,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1
-
-type DeviceType string
-
-const (
-	DeviceTypeUnknown DeviceType = ""
-)
-
-func DeviceTypePtr(c DeviceType) *DeviceType { return &c }
+package resource
