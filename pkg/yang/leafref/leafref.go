34d41e3ad0b6e17999d22bf75df9aea0ce620b71
henderiw <wim.henderickx@gmail.com>
2021-08-15 21:15:09 +0200

    added debug

--- pkg/yang/leafref/leafref.go
+++ pkg/yang/leafref/leafref.go
@@ -47,11 +47,17 @@ func NewLeafReaf(lPath, rPath *config.Path) *LeafRef {
 // ResolveLeafRefWithJSONObject resolved the leafref in the data/object supplied via the path and returns the leafref values and leafref path augmnted with the data of the leaf reference
 // we can have multiple leafrefs in an object and hence we return a list with the leafref values and the leafref path (witht the Populateed data of the object)
 func (l *LeafRef) ResolveLeafRefWithJSONObject(x1 interface{}, idx int, lridx int, resolvedLeafRefs []*ResolvedLeafRef) []*ResolvedLeafRef {
-	fmt.Printf("ResolveLeafRefWithJSONObject entry, idx %d, lridx: %d\n resolvedLeafRef: %v\n x1: %v\n", idx, lridx, resolvedLeafRefs, x1)
+	fmt.Printf("ResolveLeafRefWithJSONObject entry, idx %d, lridx: %d\n x1: %v\n", idx, lridx, x1)
+	for _, resolvedLeafRef := range resolvedLeafRefs {
+		fmt.Printf("resolvedLeafRef: %v\n", *resolvedLeafRef)
+	}
 	switch x := x1.(type) {
 	case map[string]interface{}:
 		for k, x2 := range x {
-			fmt.Printf("ResolveLeafRefWithJSONObject map[string]interface{}, idx %d, lridx: %d\n l.LocalPath: %v\n k: %s, x2: %v, resolvedLeafRef: %v\n ", idx, lridx, l.LocalPath, k, x2, resolvedLeafRefs)
+			fmt.Printf("ResolveLeafRefWithJSONObject map[string]interface{}, idx %d, lridx: %d\n l.LocalPath: %v\n k: %s, x2: %v ", idx, lridx, l.LocalPath, k, x2)
+			for _, resolvedLeafRef := range resolvedLeafRefs {
+				fmt.Printf("resolvedLeafRef: %v\n", *resolvedLeafRef)
+			}
 			if k == l.LocalPath.GetElem()[idx].GetName() {
 
 				// check if this is the last element/index in the path
@@ -85,12 +91,19 @@ func (l *LeafRef) ResolveLeafRefWithJSONObject(x1 interface{}, idx int, lridx in
 		}
 	case []interface{}:
 		resolvedLeafRefsOrig := *resolvedLeafRefs[lridx]
+		fmt.Printf("resolvedLeafRefsOrig: %v\n", resolvedLeafRefsOrig)
 		for n, v := range x {
-			fmt.Printf("ResolveLeafRefWithJSONObject []interface{}, idx %d, lridx: %d\n l.LocalPath: %v\n n: %d, v: %v\n , resolvedLeafRef: %v\n", idx, lridx, l.LocalPath, n, v, resolvedLeafRefs)
+			fmt.Printf("ResolveLeafRefWithJSONObject []interface{}, idx %d, lridx: %d\n l.LocalPath: %v\n n: %d, v: %v\n", idx, lridx, l.LocalPath, n, v)
+			for _, resolvedLeafRef := range resolvedLeafRefs {
+				fmt.Printf("resolvedLeafRef: %v\n", *resolvedLeafRef)
+			}
 			switch x2 := v.(type) {
 			case map[string]interface{}:
 				for k3, x3 := range x2 {
-					fmt.Printf("ResolveLeafRefWithJSONObject []interface{}, idx %d, lridx: %d\n l.LocalPath: %v\n n: %d, k3: %s, x3: %v\n  resolvedLeafRef: %v\n", idx, lridx, l.LocalPath, n, k3, x3, resolvedLeafRefs)
+					fmt.Printf("ResolveLeafRefWithJSONObject []interface{}, idx %d, lridx: %d\n l.LocalPath: %v\n n: %d, k3: %s, x3: %v\n ", idx, lridx, l.LocalPath, n, k3, x3)
+					for _, resolvedLeafRef := range resolvedLeafRefs {
+						fmt.Printf("resolvedLeafRef: %v\n", *resolvedLeafRef)
+					}
 					if len(l.LocalPath.GetElem()[idx].GetKey()) != 0 {
 						for k := range l.LocalPath.GetElem()[idx].GetKey() {
 							if k == k3 {
@@ -146,7 +159,7 @@ func (rlref *ResolvedLeafRef) PopulateLocalLeafRefValue(x interface{}, idx int) 
 		}
 		rlref.Resolved = true
 	case float64:
-		rlref.Value = fmt.Sprintf("%f", x1)
+		rlref.Value = fmt.Sprintf("%.0f", x1)
 		// a leaf ref can only have 1 value, this is why this works
 		for k := range rlref.LocalPath.GetElem()[idx].GetKey() {
 			rlref.LocalPath.GetElem()[idx].GetKey()[k] = fmt.Sprintf("%f", x1)
@@ -172,7 +185,7 @@ func (rlref *ResolvedLeafRef) PopulateLocalLeafRefKey(x interface{}, idx int) {
 			rlref.LocalPath.GetElem()[idx].GetKey()[k] = strconv.Itoa(int(x1))
 		}
 	case float64:
-		rlref.Value = fmt.Sprintf("%f", x1)
+		rlref.Value = fmt.Sprintf("%.0f", x1)
 		// a leaf ref can only have 1 value, this is why this works
 		for k := range rlref.LocalPath.GetElem()[idx].GetKey() {
 			rlref.LocalPath.GetElem()[idx].GetKey()[k] = fmt.Sprintf("%f", x1)
