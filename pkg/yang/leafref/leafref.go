9b5f242d89b7b8d3f1df1860d284cde669eef78b
henderiw <wim.henderickx@gmail.com>
2021-08-16 20:00:19 +0200

    fixed FindRemoteLeafRef

--- pkg/yang/leafref/leafref.go
+++ pkg/yang/leafref/leafref.go
@@ -341,23 +341,25 @@ func (rlref *ResolvedLeafRef) FindRemoteLeafRef(x1 interface{}, idx int) (found 
 						for k, value := range rlref.RemotePath.GetElem()[idx].GetKey() {
 							fmt.Printf("FindRemoteLeafRef []interface{} 2 idx: %d, k3: %v, k: %v\n", idx, k3, k)
 							if k3 == k {
+								fmt.Printf("FindRemoteLeafRef []interface{} 3 idx: %d, k3: %v, x3: %v, value: %v, rlref.Value: %v\n", idx, k3, x3, value, rlref.Value)
 								if x3 == value {
-									fmt.Printf("FindRemoteLeafRef []interface{} 3 idx: %d, k3: %v, x3: %v, rlref.Value: %v\n", idx, k3, x3, rlref.Value)
 									// check if this is the last element/index in the path
 									if idx == len(rlref.RemotePath.GetElem())-1 {
 										// return the value
-										fmt.Printf("FindRemoteLeafRef []interface{} 4 idx: %d, k3: %v, x3: %v, rlref.Value: %v\n", idx, k3, x3, rlref.Value)
+										fmt.Printf("FindRemoteLeafRef []interface{} 4 idx: %d, k3: %v, x3: %v, value: %v, rlref.Value: %v\n", idx, k3, x3, value, rlref.Value)
 										switch x := x3.(type) {
 										case string:
+											fmt.Printf("strubg a: %s, b: %s\n", string(x), value)
 											if string(x) == value {
 												return true
 											}
 										case uint32:
+											fmt.Printf("uint32 a: %s, b: %s\n", strconv.Itoa(int(x)), value)
 											if strconv.Itoa(int(x)) == value {
 												return true
 											}
 										case float64:
-											fmt.Printf("a: %s, b: %s\n", fmt.Sprintf("%.0f", x), value)
+											fmt.Printf("float64 a: %s, b: %s\n", fmt.Sprintf("%.0f", x), value)
 											if fmt.Sprintf("%.0f", x) == value {
 												return true
 											}
@@ -367,8 +369,28 @@ func (rlref *ResolvedLeafRef) FindRemoteLeafRef(x1 interface{}, idx int) (found 
 										// we should not return here since there can be multiple elements in the
 										// list and we need to exercise them all, the geenric return will take care of it
 									} else {
-										idx++
-										return rlref.FindRemoteLeafRef(x2, idx)
+										switch x := x3.(type) {
+										case string:
+											fmt.Printf("strubg a: %s, b: %s\n", string(x), value)
+											if string(x) == value {
+												idx++
+												return rlref.FindRemoteLeafRef(x2, idx)
+											}
+										case uint32:
+											fmt.Printf("uint32 a: %s, b: %s\n", strconv.Itoa(int(x)), value)
+											if strconv.Itoa(int(x)) == value {
+												idx++
+												return rlref.FindRemoteLeafRef(x2, idx)
+											}
+										case float64:
+											fmt.Printf("float64 a: %s, b: %s\n", fmt.Sprintf("%.0f", x), value)
+											if fmt.Sprintf("%.0f", x) == value {
+												idx++
+												return rlref.FindRemoteLeafRef(x2, idx)
+											}
+										default:
+											fmt.Println(reflect.TypeOf(x))
+										}
 									}
 								}
 							}
